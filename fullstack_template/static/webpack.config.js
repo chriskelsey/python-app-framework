97e044802571a9c9cf08299bec850ad947d90db2
chriskelsey <chriskelsey.net@gmail.com>
2019-10-15 18:05:31 -0700

    Initial basic framework buildout

---
+++ fullstack_template/static/webpack.config.js
@@ -0,0 +1,26 @@
+const webpack = require('webpack');
+
+const config = {
+	entry: __dirname + '/js/index.jsx',
+	output: {
+		path: __dirname + '/dist',
+		filename: 'bundle.js'
+	},
+	resolve: {
+		extensions: ['.js', '.jsx', '.css']
+	},
+	module: {
+		rules: [
+			{
+				test: /\.jsx?/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+				query:{
+     				presets: ['@babel/preset-react', '@babel/preset-env']
+   				}
+			}
+		]
+	}
+};
+
+module.exports = config;
